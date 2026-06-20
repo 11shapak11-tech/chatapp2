cbfcfaf6fa2dbbdc472e0c327d6ccf17b7af78ac
11shapak11-tech <11shapak11@gmail.com>
2026-06-21 01:50:26 +0300

    Add Firebase configuration and initialization

---
+++ mobile-app/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfpwETQSX_aEiOgVfymWePc3HHVP84VT8",
+  authDomain: "mersal-886de.firebaseapp.com",
+  projectId: "mersal-886de",
+  storageBucket: "mersal-886de.firebasestorage.app",
+  messagingSenderId: "276755607104",
+  appId: "1:276755607104:web:bdaca5ce627a97d1e3b9a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
